62ade2425041899254e5c00f793ba060c01a1023
aaqib-kk <aqib.khan.25@iiee.edu.pk>
2020-06-30 19:01:01 +0500

    k

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,32 @@
 
 //==========================
 
-use surf;
-use futures::try_join;
-use async_std::task;
+// use surf;
+// use futures::try_join;
+// use async_std::task;
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {         // box is used for error handling
-    task::block_on(async {
-        let req1 = surf::get("https://www.google.com/").recv_string();
-        let req2 = surf::get("https://www.google.com/").recv_string();
+// fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {         // box is used for error handling
+//     task::block_on(async {
+//         let req1 = surf::get("https://www.google.com/").recv_string();
+//         let req2 = surf::get("https://www.google.com/").recv_string();
 
-        let (str1, str2) = futures::future::try_join(req1, req2).await?;
-        dbg!("{:?}", str2);     // its a debug for printing present in surf
-        dbg!("{:?}", str1);
-        Ok(())
-    })
+//         let (str1, str2) = futures::future::try_join(req1, req2).await?;
+//         dbg!("{:?}", str2);     // its a debug for printing present in surf
+//         dbg!("{:?}", str1);
+//         Ok(())
+//     })
+// }
+
+//========================
+
+fn main() {
+    match reqwest::get("https://httpbin.org/ip") {
+        Ok(mut res) => {
+            match res.text() {
+                Ok(text) => println!("response is: {:?}", text),
+                Err(_) => println!("The error")
+            }
+        }
+        Err(_) => println!("The error")
+    }
 }
